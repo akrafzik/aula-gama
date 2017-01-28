78d60467e2ace20594b72b3ceca42aab98557099
akrafzik <alec.k.haroutiounian@hotmail.com>
2017-01-28 14:36:52 -0200

    update

--- challenge/game/game_test.rb
+++ challenge/game/game_test.rb
@@ -3,4 +3,19 @@ require 'minitest/autorun'
 
 class GameTest < MiniTest::Unit::TestCase
 
+	def test_sortear
+	numero_sorteado = Game.new.sortear
+	assert_kind_of(Integer, numero_sorteado)
+	assert_includes(1..3, numero_sorteado)
+end
+
+	def test_chutar_certo
+	chute = Game.new.chutar(2, 2)
+	assert_equal("Acertou!!!!", chute)
+end
+	def test_chutar_errado
+	chute = Game.new.chutar(2, 6)
+	assert_equal("ERRRRRRROOOOUUUU!", chute)
+end
+
 end
